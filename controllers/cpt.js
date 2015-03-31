a0a7d61fd9831cad164f03ec96d0cc9b26b9e1d8
Alexander Price <alexander@Alexanders-MacBook-Pro.local>
2015-03-30 19:41:20 -0500

    got server setup

---
+++ controllers/cpt.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var CPT = require('../models/cpt.js');
+
+module.exports = {
+
+};
+
---
+++ models/cpt.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var Q = require('q');
+
+
+
+
+var CPT = mongoose.model('CPT', new Schema({
+}));
+
+module.export = CPT;
+
