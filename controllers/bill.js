f79d6ef1070c4882ca84887c0a42db5ad0affc12
vattila2049@yahoo.com <vattila2049@yahoo.com>
2015-03-30 20:33:25 -0500

    api endpoint wireframes

--- controllers/bill.js
+++ controllers/bill.js
@@ -4,5 +4,16 @@ var Bill = require('../models/bill.js');
 
 module.exports = {
 
+  single: function(req, res, next) {
+	res.send('single');
+  },
+
+  insertNew: function(req, res, next) {
+	res.send('insertNew');
+  },
+
+  index: function(req, res, next) {
+	res.send('index');
+  }
 };
 
--- controllers/cpt.js
+++ controllers/cpt.js
@@ -4,5 +4,13 @@ var CPT = require('../models/cpt.js');
 
 module.exports = {
 
+  single: function(req, res, next) {
+	res.send('single');
+  },
+
+  search: function(req, res, next) {
+	res.send('search');
+  }
+
 };
 
